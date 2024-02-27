bf58e41da35dde0833fa84566ee67714aa3560fd
Negima1072 <ngm1072@gmail.com>
2024-02-28 02:09:09 +0900

    fix

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import { App } from "./App";
-import "./global.css";
+import "./styles.css";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
--- webpack.config.ts
+++ webpack.config.ts
@@ -55,26 +55,16 @@ const preload: Configuration = {
 
 const renderer: Configuration = {
   ...common,
-  // セキュリティ対策として 'electron-renderer' ターゲットは使用しない
   target: 'web',
   entry: {
-    // React アプリのエントリーファイル
-    app: './src/client/index.tsx',
+    app: './src/client/main.tsx',
   },
   plugins: [
-    // CSS を JS へバンドルせず別ファイルとして出力するプラグイン
     new MiniCssExtractPlugin(),
-    /**
-     * バンドルしたJSファイルを <script></scrip> タグとして差し込んだ
-     * HTMLファイルを出力するプラグイン
-     */
     new HtmlWebpackPlugin({
-      // テンプレート
       template: './src/client/index.html',
     }),
   ],
 };
 
-// 上記 3 つの設定を配列にしてデフォルト・エクスポート
-// ただし開発時にはレンダラープロセスのみをバンドルし、メイン or プリロードは tsc に処理させる
 export default isDev ? renderer : [main, preload, renderer];
