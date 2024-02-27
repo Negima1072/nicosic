c9bde884bc2a3b03855f0b32e46498fdc0b114c7
Negima1072 <ngm1072@gmail.com>
2024-02-28 01:59:15 +0900

    fix

--- src/main.ts
+++ src/main.ts
@@ -21,10 +21,15 @@ if (process.env.NODE_ENV === "development") {
     });
 }
 
+if (process.env.NODE_ENV !== "development" || app.isPackaged) {
+    app.disableHardwareAcceleration();
+}
+
 app.whenReady().then(() => {
     const mainWindow = new BrowserWindow({
         webPreferences: {
             preload: path.join(__dirname, "preload.js"),
+            devTools: process.env.NODE_ENV === "development",
         },
     });
 
