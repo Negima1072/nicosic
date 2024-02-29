caa6161c73b67da3c2ab1cbe73b058ffc1b21099
Negima1072 <ngm1072@gmail.com>
2024-02-29 16:32:45 +0900

    add: back btn

--- src/client/components/MainFrame/MainFrame.tsx
+++ src/client/components/MainFrame/MainFrame.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import { HomePage } from "../pages/HomePage/HomePage";
 import { MylistDetailPage } from "../pages/MylistDetailPage/MylistDetailPage";
 import { MylistPage } from "../pages/MylistPage/MylistPage";
@@ -7,19 +7,31 @@ import { SearchPage } from "../pages/SearchPage/SearchPage";
 import { UserPage } from "../pages/UserPage/UserPage";
 import styled from "./MainFrame.module.scss";
 import { ConfigPage } from "../pages/ConfigPage/ConfigPage";
+import { MdArrowBackIos } from "react-icons/md";
 
 export const MainFrame = () => {
+    const navigate = useNavigate();
+    const backBtnHandler = () => {
+        navigate(-1);
+    }
     return (
         <div className={styled.mainFrame}>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/ranking" element={<RankingPage />} />
-                <Route path="/search" element={<SearchPage />} />
-                <Route path="/mylist" element={<MylistPage />} />
-                <Route path="/mylist/:mylistId" element={<MylistDetailPage />} />
-                <Route path="/user/:userId" element={<UserPage />} />
-                <Route path="/config" element={<ConfigPage />} />
-            </Routes>
+            <div className={styled.header}>
+                <button onClick={backBtnHandler} className={styled.backBtn}>
+                    <MdArrowBackIos />
+                </button>
+            </div>
+            <div className={styled.content}>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/ranking" element={<RankingPage />} />
+                    <Route path="/search" element={<SearchPage />} />
+                    <Route path="/mylist" element={<MylistPage />} />
+                    <Route path="/mylist/:mylistId" element={<MylistDetailPage />} />
+                    <Route path="/user/:userId" element={<UserPage />} />
+                    <Route path="/config" element={<ConfigPage />} />
+                </Routes>
+            </div>
         </div>
     );
 };
