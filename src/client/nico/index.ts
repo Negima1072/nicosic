12ad438f381c36d635e6bfdfc0739fad646f5f87
Negima1072 <ngm1072@gmail.com>
2024-03-03 20:06:38 +0900

    fix: nico api

--- src/client/nico/index.ts
+++ src/client/nico/index.ts
@@ -1,3 +1,7 @@
 export * as common from "./common";
 export * as user from "./user";
 export * as video from "./video";
+export * as list from "./list";
+export * as ranking from "./ranking";
+export * as recommend from "./recommend";
+export * as search from "./search";
